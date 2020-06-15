a5b01f46bde0cf00296ba6611737ffcf5c8c9ebe
Sam Clark <samuel.clark@uvm.edu>
2020-06-14 22:21:06 -0400

    commands

--- void_bot.py
+++ void_bot.py
@@ -8,7 +8,6 @@ class VoidBot(discord.Client):
         self.socketio = socketio
 
         self.messages = []  # [(ip, message)]
-
         asyncio.get_event_loop().create_task(self.send_queued_messages())
 
     def queue_message(self, ip, message):
@@ -21,11 +20,11 @@ class VoidBot(discord.Client):
                 await self.send_message(message[0], message[1])
             await asyncio.sleep(0.5)
 
-    async def send_message(self, ip, message):
+    async def send_message(self, channel, message):
         guild: discord.guild.Guild = self.guilds[0]
-        channel: discord.guild.TextChannel = discord.utils.get(guild.channels, name=ip)
+        channel: discord.guild.TextChannel = discord.utils.get(guild.channels, name=channel)
         if not channel:
-            channel = await guild.create_text_channel(ip)
+            channel = await guild.create_text_channel(channel)
         await channel.send(message)
 
     async def on_ready(self):
@@ -34,7 +33,15 @@ class VoidBot(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        self.socketio.emit("message", message.content)
+        elif message.content[0] == "!":
+            await self.handle_command(message)
+        else:
+            self.socketio.emit("message", message.content)
+
+    async def handle_command(self, command):
+        print(command)
+        await self.send_message("general", command)
+
 
 
 if __name__ == '__main__':
